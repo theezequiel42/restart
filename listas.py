756975e0fdfe7bf734aef9ec4ceb36e8bfae8cb0
EZEQUIEL ANTUNES DE LIMA <94005353+theezequiel42@users.noreply.github.com>
2026-02-01 23:58:35 -0300

    trabalhado iteração com múltiplas listas

--- listas.py
+++ listas.py
@@ -16,3 +16,12 @@ palavras = ['maçã', 'banana', 'laranja', 'uva']
 for palavra in palavras:
     if len(palavra) >= 5:
         print(f'A palavra "{palavra}" tem 5 ou mais letras.')
+
+
+valores = [2, 4, 6]
+outros_valores = [1, 8, 6, 2]
+
+for valor in outros_valores:
+    for v in valores:
+        if valor == v:
+            print(f'O valor {valor} está presente em ambos os arrays.')
